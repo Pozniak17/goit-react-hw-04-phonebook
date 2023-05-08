72a91f4022bc832a8cf8d01f935c24c028bd8c4f
Yevhenii Pozniak <jekamanu@gmail.com>
2023-05-08 13:54:47 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
-
-import { App } from 'components/App';
+// import ReactDOM from 'react-dom/client';
+import 'modern-normalize';
 import { ThemeProvider } from 'styled-components';
-import { theme } from './theme';
+import { App } from 'components/App/App';
+import { theme } from 'utils/theme';
+import { MyStyleReset } from 'utils/resetStyles';
 
 // import ReactDOM from 'react-dom/client'; <- This import is only // for React version 18
 import { render } from 'react-dom'; // <- This is the correct import // statement for React version 17
-
 // const root = //ReactDOM.createRoot(document.getElementById('root'));
 const root = document.getElementById('root'); // <- This is the //correct method call for React version 17
 render(
   <React.StrictMode>
+    <MyStyleReset />
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
--- src/utils/theme.js
+++ src/utils/theme.js
@@ -2,12 +2,17 @@ export const theme = {
   colors: {
     black: '#000',
     white: '#fff',
-    text: '#19100B',
-    background: '#fcf5e4',
-    primary: '#c5a78a',
-    secondary: '#6f4e37',
-    accent: '#3A2113',
+    grey: 'gray',
+    red: 'red',
+    green: 'green',
+    blue: '#3b96dcf3',
+    text: '#2a2a2a',
+    background: '#fff',
+    primary: '#07c',
+    secondary: '#05a',
+    accent: '#609',
     muted: '#f6f6f6',
+    tableAccent: '#2e729d33',
   },
   space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
   fonts: {
@@ -17,13 +22,14 @@ export const theme = {
   },
   fontSizes: {
     xs: '12px',
-    s: '14px',
-    m: '16px',
+    s: '16px',
+    m: '24px',
     l: '32px',
     xl: '64px',
   },
   fontWeights: {
     normal: 400,
+    semiBold: 500,
     bold: 700,
   },
   lineHeights: {
@@ -33,6 +39,7 @@ export const theme = {
   borders: {
     none: 'none',
     normal: '1px solid',
+    gray: '1px solid rgba(34, 60, 80, 0.3)',
   },
   radii: {
     none: '0',
@@ -40,5 +47,8 @@ export const theme = {
     md: '8px',
     lg: '16px',
     round: '50%',
+  },
+  shadows: {
+    normal: '5px 5px 5px -5px rgba(34, 60, 80, 0.5)',
   },
 };

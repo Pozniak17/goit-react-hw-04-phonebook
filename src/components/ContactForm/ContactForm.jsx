02c2b1ce500ca26912926f074e80c614e991f5a1
Yevhenii Pozniak <jekamanu@gmail.com>
2023-10-01 12:44:31 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import {
   FormContainer,
   FormLabel,
@@ -6,67 +6,67 @@ import {
   Button,
 } from './ContactForm.styled';
 
-export class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+export const ContactForm = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
   // обробник подій за атрибутом name✅
-  handleChange = event => {
+  // якщо значення поля співпадає, записуємо значення в name/number
+  const handleChange = event => {
+    // setState({[name]:value}) --- в name запиши значення, в number запиши
     const { name, value } = event.target;
-    this.setState({ [name]: value });
+
+    console.log(name);
+    if (name === 'name') {
+      setName(value);
+    } else if (name === 'number') {
+      setNumber(value);
+    }
   };
 
-  handleSumbit = event => {
-    const { name, number } = this.state;
+  const handleSumbit = event => {
     event.preventDefault();
-
-    this.props.onSubmit(name, number);
-    this.reset();
+    // передаємо через проп onSumit => App
+    onSubmit(name, number);
+    reset();
   };
 
-  reset = () => {
-    this.setState({
-      name: '',
-      number: '',
-    });
+  const reset = () => {
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    const { name, number } = this.state;
-    return (
-      <FormContainer onSubmit={this.handleSumbit}>
-        <FormLabel>
-          Name
-          <FormInput
-            value={name}
-            onChange={this.handleChange}
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            placeholder="Name"
-          />
-        </FormLabel>
+  return (
+    <FormContainer onSubmit={handleSumbit}>
+      <FormLabel>
+        Name
+        <FormInput
+          value={name}
+          onChange={handleChange}
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          placeholder="Name"
+        />
+      </FormLabel>
 
-        <FormLabel>
-          Number
-          <FormInput
-            value={number}
-            onChange={this.handleChange}
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            placeholder="Nubmer"
-          />
-        </FormLabel>
+      <FormLabel>
+        Number
+        <FormInput
+          value={number}
+          onChange={handleChange}
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          placeholder="Nubmer"
+        />
+      </FormLabel>
 
-        <Button type="submit">Add contact</Button>
-      </FormContainer>
-    );
-  }
-}
+      <Button type="submit">Add contact</Button>
+    </FormContainer>
+  );
+};

e878d0301eebbb30e439668adbbdd241ea4e7de7
Yevhenii Pozniak <jekamanu@gmail.com>
2023-10-12 15:49:24 +0300

    Fixed getItem localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,12 +9,16 @@ import { Layout } from './Layout.styled';
 
 export const App = () => {
   const [filter, setFilter] = useState('');
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(
+    JSON.parse(
+      window.localStorage.getItem('contacts') ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+    )
+  );
 
   useEffect(() => {
     const savedContacts = localStorage.getItem('contacts');
@@ -26,7 +30,7 @@ export const App = () => {
   }, []);
 
   useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
   const addContact = (name, number) => {
